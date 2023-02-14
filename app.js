791f71d3e346aacdfccdf218dea4903f3133aa21
DelrosarioRovic <rovicdelrosario01@gmail.com>
2023-02-14 12:41:44 +0800

    preparing to upload in hosting site

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const mongooseEcryption = require('mongoose-encryption');
+
+// cookies and session
+const passportLocalMongoose = require('passport-local-mongoose');
+const session = require('express-session');
+const passport = require('passport');
+//google oauth
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -12,21 +19,68 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://127.0.0.1:27017/userPracDB',{ useNewUrlParser: true});
+// mongoose.connect('mongodb://127.0.0.1:27017/userPracDB',{ useNewUrlParser: true});
+mongoose.connect('mongodb+srv://Akocrovic123:Akocrovic123@mydb.jfyc7mf.mongodb.net/userPracDB',{ useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String
 });
-const secret = process.env.SECRET;
-
-userSchema.plugin(mongooseEcryption, {secret: secret, encryptedFields: ['password'] });
+// const secret = process.env.SECRET;
+// userSchema.plugin(mongooseEcryption, {secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 const User = new mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+})
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 
 
 app.get("/", function(req, res) {
     res.render("home");
+});
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+  );
+
+app.get('/auth/google/secrets', 
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
 });
 
 app.get("/login", function(req, res) {
@@ -37,35 +91,79 @@ app.get("/register", function(req, res) {
     res.render("register");
 });
 
+app.get("/submit", function(req, res) {
+    if (req.isAuthenticated()) {
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/secrets", function(req, res) {
+    User.find({"secret": {$ne: null}}, function(err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                res.render("secrets", {usersWithSecrets: foundUser});
+            }
+        }
+    });
+});
+
+app.get("/logout", function(req, res) {
+    req.logout(function(err) {
+        if (err) {
+          console.error(err);
+          return res.status(500).send('An error occurred while logging out.');
+        }
+        req.session.destroy(function (err) {
+            res.redirect("/");
+          });
+      });
+});
+
 app.post("/register", function(req, res) {
-    const newUser = new User({
-        email: req.body.username,
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if (err) {
+          console.log(err);
+          res.redirect("/register");  
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            })
+        }
+    })
+});
+
+app.post("/login", function(req, res) {
+    const user = new User ({
+        username: req.body.username,
         password: req.body.password
     });
 
-    newUser.save(function(err) {
+    req.login(user, function(err) {
         if (err) {
             console.log(err);
         } else {
-            console.log("Successfully register");
-            res.render("secrets");
+            passport.authenticate("local", { failureRedirect: '/login' })(req, res, function() {
+                res.redirect("/secrets");
+            });
         }
-    })
-
+    });
 });
-app.post("/login", function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({email:username}, function(err, foundUser) {
+app.post("/submit", function(req, res) {
+    const newSecret = req.body.secret;
+    console.log(req.user.id);
+    User.findById(req.user.id, function(err, foundUser) {
         if (err) {
             console.log(err);
         } else {
-            if (foundUser.email === username && foundUser.password === password) {
-                res.render("secrets");
-            } else {
-                res.render("login");
-                
+            if (foundUser) {
+                foundUser.secret = newSecret;
+                foundUser.save(function() {
+                    res.redirect("/secrets")
+                })
             }
         }
     })
@@ -73,8 +171,10 @@ app.post("/login", function(req, res) {
 
 
 
-
-
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 app.listen(3000, function() {
-    console.log("Server Started on port 3000.");
+    console.log("The Server is running successfully");
 })
