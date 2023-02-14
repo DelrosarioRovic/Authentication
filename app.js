1d47ffd8ad678eb97e39af3778a203caa749fb81
Rovic <118983530+DelrosarioRovic@users.noreply.github.com>
2023-02-14 15:11:17 +0800

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://whatsyoursecrets.cyclic.app/auth/google/callback",
+    callbackURL: "https://whatsyoursecrets.cyclic.app/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
